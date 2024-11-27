ebe4d683fde8ddcd7f9e2a48179a494608adc6ad
BolNick <BolNick09RUS@gmail.com>
2024-11-27 17:42:51 +0300

    Добавьте файлы проекта.

---
+++ WpfTextEditor/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSemibold_Click(object sender, RoutedEventArgs e)
+        {
+            if (richTextBox.Selection != null)
+            {
+                richTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+        }
+
+        private void btnColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (richTextBox.Selection != null)
+            {
+                richTextBox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+            }
+        }
+    }
+}
